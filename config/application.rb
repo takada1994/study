40d98f6f09619ad773b84692d8cd27b5a91f31d7
takada1994 <takadanaritoshi@gmail.com>
2020-08-29 06:09:37 +0900

    エラーメッセージを国際化

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Myproject
     config.load_defaults 5.2
 
     config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
 
     config.action_controller.permit_all_parameters = true
 
