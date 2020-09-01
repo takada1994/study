c06c92fc43d3c5eabf5eaa5dcb89054c6671371a
takada1994 <takadanaritoshi@gmail.com>
2020-09-01 12:41:27 +0900

    サイドバーにブログ記事を掲載

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,18 @@
 class EntriesController < ApplicationController
   def index
+    if params[:member_id]
+      @member = Member.find(params[:member_id])
+      @entries = @member.entries
+    else
+      @entries = Entry.all
   end
 
+  @entries = @entries.readable_for(current_member)
+    .order(posted_at: :desc).page(params[:page]).per(3)
+end
+
   def show
+    @entry = Entry.readable_for(current_member).find(params[:id])
   end
 
   def new
