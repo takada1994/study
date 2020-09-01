0d371dce80e78fb955b79a1f2e420463ec057346
takada1994 <takadanaritoshi@gmail.com>
2020-09-01 13:07:32 +0900

    ブログ記事の編集、削除機能を実装

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -16,8 +16,37 @@ end
   end
 
   def new
+    @entry = Entry.new(posted_at: Time.current)
   end
 
   def edit
+    @entry = current_member.entries.find(params[:id])
   end
+
+  def create
+    @entry = Entry.new(params[:entry])
+    @entry.author = current_member
+    if @entry.save
+      redirect_to @entry, notice: "記事を更新しました"
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @entry = current_member.entries.find(params[:id])
+    @entry.assign_attributes(params[:entry])
+    if @entry.save
+      redirect_to @entry, notice: "記事を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @entry = current_member.entries.find(params[:id])
+    @entry.destroy
+    redirect_to :entries, notice: "記事を更新しました。"
+  end
+
 end
