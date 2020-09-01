697b4b65c4960fc590f158c2b0a6766a64fbd235
takada1994 <takadanaritoshi@gmail.com>
2020-09-01 16:39:16 +0900

    パスワード変更機能をストロングパラメータに対応

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -32,7 +32,7 @@ class MembersController < ApplicationController
   
     # 会員の新規登録
     def create
-      @member = Member.new(params[:member])
+      @member = Member.new(member_params)
       if @member.save
         redirect_to @member, notice: "会員を登録しました。"
       else
@@ -43,7 +43,7 @@ class MembersController < ApplicationController
     # 会員情報の更新
     def update
       @member = Member.find(params[:id])
-      @member.assign_attributes(params[:member])
+      @member.assign_attributes(member_params)
       if @member.save
         redirect_to @member, notice: "会員情報を更新しました。"
       else
@@ -57,4 +57,20 @@ class MembersController < ApplicationController
       @member.destroy
       redirect_to :members, notice: "会員を削除しました。"
     end
+
+  private def member_params
+    attrs = [
+      :number,
+      :name,
+      :full_name,
+      :sex,
+      :birthday,
+      :email,
+      :administrator
+    ]
+
+    attrs << :password if params[:action] == "create"
+
+    params.require(:member).permit(attrs)
   end
+end
