30de72db650ea7f4a4dab2c06d30669de942aeaa
takada1994 <takadanaritoshi@gmail.com>
2020-09-02 11:13:38 +0900

    プロフィール画像のアップロード機能を追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -27,6 +27,22 @@ class Member < ApplicationRecord
   attr_accessor :current_password
   validates :password, presence: { if: :current_password }
 
+  validate if: :new_profile_picture do
+    if new_profile_picture.respond_to?(:content_type)
+      unless new_profile_picture.content_type.in?(ALLOWED_CONTENT_TYPES)
+        errors.add(:new_profile_picture, :invalid_image_type)
+      end
+    else
+      errors.add(:new_profile_picture, :invalid)
+    end
+  end
+
+  before_save do
+    if new_profile_picture
+      self.profile_picture = new_profile_picture
+    end
+  end
+
   class << self
     def search(query)
       rel = order("number")
